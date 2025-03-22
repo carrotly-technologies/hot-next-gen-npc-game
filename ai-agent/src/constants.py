7bd6c807ff6264a74f38d38ff24522738ad2da15
matjamr <jamr.mat@gmail.com>
2025-03-22 17:27:44 +0100

    sample rest transaction

---
+++ ai-agent/src/constants.py
@@ -0,0 +1,21 @@
+import os
+
+employees_data = {
+    "EmployeeID": "",
+    "FirstName": "",
+    "LastName": "",
+    "BirthDate": "",
+    "Salary": 0,
+}
+
+db_params = {
+    "dbname": "HOT",
+    "user": "USER",
+    "password": "PASSWORD",
+    "host": "localhost",
+    "port": "5432",
+}
+
+DB_FETCH_AGENT_ADDRESS = (
+    "agent1qwg0h3gx2kvqmwadlg0j4r258r7amcfskx2mudz92ztjmtfdclygxrh5esu"
+)
