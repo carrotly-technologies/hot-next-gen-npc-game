4772a6ff3df2aed9802ad6e001b048fe45f87dc3
matjamr <jamr.mat@gmail.com>
2025-03-22 14:54:43 +0100

    changes for map

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python
-"""pygame.examples.moveit
-
-This is the full and final example from the Pygame Tutorial,
-"How Do I Make It Move". It creates 10 objects and animates
-them on the screen.
-
-It also has a separate player character that can be controlled with arrow keys.
-
-Note it's a bit scant on error checking, but it's easy to read. :]
-Fortunately, this is python, and we needn't wrestle with a pile of
-error codes.
-"""
 
 import os
+from os.path import join
+
 import pygame
-from pygame.math import Vector2
+
+from loaders import load_all_characters, tmx_importer
 from player import Player
-from sprites import Sprites
-from loaders import load_all_characters
 from settings import *
+from sprites import Sprites, Sprite, CollidableSprite, BorderSprite
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 
@@ -40,10 +30,33 @@ class Game:
 		self.tint_direction = -1
 		self.tint_speed = 600
 
+		# spirtes
 		self.sprites = Sprites()
+		self.collision_sprites = pygame.sprite.Group()
+		self.character_sprites = pygame.sprite.Group()
+		self.transition_sprites = pygame.sprite.Group()
+
+		# transition / tint
+		self.transition_target = None
+		self.tint_surf = pygame.Surface((WIDTH, HEIGHT))
+		self.tint_mode = 'untint'
+		self.tint_progress = 0
+		self.tint_direction = -1
+		self.tint_speed = 600
 
 		self.load_assets()
-		self.player = Player(Vector2(0, 0), self.frames['characters']['fire_boss'], self.sprites)
+		self.import_assets()
+		self.setup(self.tmx_maps['world_map'], 'house')
+
+	def import_assets(self):
+		self.tmx_maps = tmx_importer('data', 'maps')
+
+		self.fonts = {
+			'dialog': pygame.font.Font(join('data', 'graphics', 'fonts', 'PixeloidSans.ttf'), 30),
+			'regular': pygame.font.Font(join('data', 'graphics', 'fonts', 'PixeloidSans.ttf'), 18),
+			'small': pygame.font.Font(join('data', 'graphics', 'fonts', 'PixeloidSans.ttf'), 14),
+			'bold': pygame.font.Font(join('data', 'graphics', 'fonts', 'dogicapixelbold.otf'), 20),
+		}
 
 	def load_assets(self):
 		self.frames = { 'characters': load_all_characters('data', 'graphics', 'characters') }
@@ -66,10 +79,49 @@ class Game:
 		self.tint_surf.set_alpha(self.tint_progress)
 		self.display_surface.blit(self.tint_surf, (0,0))
 
+	def setup(self, tmx_map, player_start_pos):
+		# clear the map
+		for group in (self.sprites, self.collision_sprites, self.transition_sprites, self.character_sprites):
+			group.empty()
+
+		# terrain
+		for layer in ['Terrain']:
+			for x, y, surf in tmx_map.get_layer_by_name(layer).tiles():
+				Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.sprites, WORLD_LAYERS['bg'])
+
+		# coast
+		# for obj in tmx_map.get_layer_by_name('Coast'):
+		# 	terrain = obj.properties['terrain']
+		# 	side = obj.properties['side']
+		# 	AnimatedSprite((obj.x, obj.y), self.overworld_frames['coast'][terrain][side], self.sprites,
+		# 				   WORLD_LAYERS['bg'])
+
+		# objects
+		for obj in tmx_map.get_layer_by_name('Objects'):
+			if obj.name == 'top':
+				Sprite((obj.x, obj.y), obj.image, self.sprites, WORLD_LAYERS['top'])
+			else:
+				CollidableSprite((obj.x, obj.y), obj.image, (self.sprites, self.collision_sprites))
+
+		# transition objects
+		# for obj in tmx_map.get_layer_by_name('Transition'):
+		# 	TransitionSprite((obj.x, obj.y), (obj.width, obj.height), (obj.properties['target'], obj.properties['pos']),
+		# 					 self.transition_sprites)
+
+		# collision objects
+		for obj in tmx_map.get_layer_by_name('Collisions'):
+			BorderSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+
+		# entities
+		for obj in tmx_map.get_layer_by_name('Entities'):
+			if obj.name == 'Player':
+				if obj.properties['pos'] == player_start_pos:
+					self.player = Player((obj.x, obj.y), self.frames['characters']['fire_boss'], self.sprites)
+
 	def run(self):
 		while True:
 			dt = self.clock.tick() / 1000
-			self.display_surface.fill('black')
+			self.display_surface.fill('pink')
 
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
